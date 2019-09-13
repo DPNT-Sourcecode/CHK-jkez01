f36c47294d5a6920d91b1df0d73d2c9e939b4b33
sbx_user1051 <sbx_user1051@169.254.102.37>
2019-09-13 03:46:14 +0000

    Fri Sep 13 03:46:14 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,7 +12,11 @@ class SpecialOffer:
     Describes a special offer for an SKU
     Types can be any of OfferType, discount describes what the offer is
     """
-    #def get_
+    def __get_cost__(self, price_table):
+        return {
+            OfferType.DISCOUNT : lambda: self.__get_discount_cost__(discount),
+            OfferType.COMBO : lambda: self.__get_combo_cost__(discount) 
+        }.get(self.type, lambda: raise_value_error("Invalid SpecialOffer OfferType"))()
 
     def __set_discount__(self, discount):
         if len(discount) < 2:
@@ -23,7 +27,7 @@ class SpecialOffer:
     def __set_combo__(self, discount):
         if len(discount) < 2:
             raise ValueError("Invalid COMBO type")
-        self.sku = discount[0]
+        self.discounted_sku = discount[0]
         self.ammount_free = discount[1]
 
     def __set_type__(self, discount):
@@ -32,7 +36,8 @@ class SpecialOffer:
             OfferType.COMBO : lambda: self.__set_combo__(discount) 
         }.get(self.type, lambda: raise_value_error("Invalid SpecialOffer OfferType"))()
 
-    def __init__(self, type, discount):
+    def __init__(self, sku, type, discount):
+        self.sku = sku
         self.type = type
         self.discount = self.__set_type__(discount)
 
@@ -49,10 +54,10 @@ class PriceTable:
         self.price_table = {'C': (20, False), 'D': (15, False), 'B':(30, True),
         'E': (40, True), 'A': (50, True)}
         self.special_offers = []
-        self.special_offers.append((SpecialOffer(OfferType.DISCOUNT, (2, 45)),'B'))
-        self.special_offers.append((SpecialOffer(OfferType.COMBO, ('B', 1)),'E'))
-        self.special_offers.append((SpecialOffer(OfferType.DISCOUNT, (3, 130)),'A'))
-        self.special_offers.append((SpecialOffer(OfferType.DISCOUNT, (5, 200)),'A'))
+        self.special_offers.append((SpecialOffer('B', OfferType.DISCOUNT, (2, 45))))
+        self.special_offers.append((SpecialOffer('E', OfferType.COMBO, ('B', 1))))
+        self.special_offers.append((SpecialOffer('A', OfferType.DISCOUNT, (3, 130))))
+        self.special_offers.append((SpecialOffer('A', OfferType.DISCOUNT, (5, 200))))
         
 
 # noinspection PyUnusedLocal
@@ -80,6 +85,7 @@ def checkout(skus):
         return total
     except Exception as e:
         return -1
+
 
 
 

de2ca9654a26fb7bbc2075b3e024fc23e6aa4410
sbx_user1051 <sbx_user1051@169.254.128.221>
2019-09-13 02:05:14 +0000

    Fri Sep 13 02:05:14 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -6,4 +6,23 @@ class TestCheckout():
     def test_one_each(self):
         assert checkout_solution.checkout("ABCD") == 115
 
+    def test_one_product(self):
+        assert checkout_solution.checkout("A") == 50
+
+    def test_special(self):
+        assert checkout_solution.checkout("AAA") == 130
+
+    def test_special_plus_one(self):
+        assert checkout_solution.checkout("AAAA") == 180
+
+    def test_bad_none(self):
+        assert checkout_solution.checkout("") == -1
+
+    def test_bad_unknown(self):
+        assert checkout_solution.checkout("E") == -1
+
+    def test_bad_type(self):
+        assert checkout_solution.checkout(3) == -1
+
+
 

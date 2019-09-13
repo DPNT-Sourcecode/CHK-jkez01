0a92f43efb7f55b6e45eb5d259884a1bd3520bc9
sbx_user1051 <sbx_user1051@169.254.128.221>
2019-09-13 02:08:14 +0000

    Fri Sep 13 02:08:14 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,3 +3,18 @@
 def checkout(skus):
     return -1
 
+def get_price_table_and_offers():
+    """
+    Return a price dictionary:
+    skus are the keys, values are a (price, special) tuple
+    price is a number, while special is a tuple of (n,y)
+    where n is n of that sku costs y pounds
+    if there is no special offer, special is None
+    """
+    prices = {'A': (50, (3,130)),
+              'B': (30, (2,45)),
+              'C': (30, None),
+              'D': (30, None)}
+    return prices
+
+

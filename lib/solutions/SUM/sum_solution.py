8c2e50554055c7cc9e7d006d160bddf5f3a75867
sbx_user1051 <sbx_user1051@169.254.128.221>
2019-09-13 01:42:08 +0000

    Fri Sep 13 01:42:08 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,9 +1,10 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
     try:
-        if x < 0 or x > 100 or y < 0 or y > 100:
+        if x < 0 or x > 100 or y < 0 or y > 100 or int(x) != x or int(y) != y:
             raise ValueError("Parameters must be positive integers between 0-100")
-        return x + y
+        return int(x) + int(y)
     except Exception as e:
         raise(e)
+
 

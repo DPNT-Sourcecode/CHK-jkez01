3ee1e74f5ed18b71f2d78939109cf2ad4a0ef488
sbx_user1051 <sbx_user1051@169.254.71.241>
2019-09-13 02:55:46 +0000

    Fri Sep 13 02:55:46 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,27 +3,40 @@ import pytest
 
 
 class TestCheckout():
+    def test_none(self):
+        assert checkout_solution.checkout("") == 0
+        
     def test_one_each(self):
-        assert checkout_solution.checkout("ABCD") == 115
-
-    def test_one_product(self):
-        assert checkout_solution.checkout("A") == 50
-
-    def test_special(self):
-        assert checkout_solution.checkout("AAA") == 130
+        assert checkout_solution.checkout("ABCDE") == 155
 
     def test_non_special(self):
         assert checkout_solution.checkout("C") == 20
 
-    def test_special_plus_one(self):
-        assert checkout_solution.checkout("AAAA") == 180
+    def test_special_a_one(self):
+        assert checkout_solution.checkout("AAA") == 130
 
-    def test_bad_none(self):
-        assert checkout_solution.checkout("") == 0
+    def test_special_a_two(self):
+        assert checkout_solution.checkout("AAAAA") == 200
+
+    def test_special_a_both(self):
+        assert checkout_solution.checkout("AAAAAAAA") == 430
+
+    def test_special_b_one(self):
+        assert checkout_solution.checkout("BB") == 45
+
+    def test_special_e_one_no_b(self):
+        assert checkout_solution.checkout("EE") == 80
+
+    def test_special_e_one_with_one_b(self):
+        assert checkout_solution.checkout("EEB") == 80
+
+    def test_special_e_one_with_two_b(self):
+        assert checkout_solution.checkout("EEBB") == 110
 
     def test_bad_unknown(self):
-        assert checkout_solution.checkout("E") == -1
+        assert checkout_solution.checkout("F") == -1
 
     def test_bad_type(self):
         assert checkout_solution.checkout(3) == -1
+
 

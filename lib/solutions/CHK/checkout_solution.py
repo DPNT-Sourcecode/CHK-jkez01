051a23a59816f812579811e8bacee732fe1020b1
sbx_user1051 <sbx_user1051@169.254.128.221>
2019-09-13 02:23:14 +0000

    Fri Sep 13 02:23:14 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,30 +6,40 @@ def checkout(skus):
     special_item_counter = {}
     try:
         for item in skus:
-            if item not in price_data:
+            if item not in price_data.price_table:
                 return -1
-            if price_data[item][1] is None:
-                total += price_data[item][0]
+            if price_data.price_table[item][1] is None:
+                total += price_data.price_table[item][0]
             elif item in special_item_counter:
                 special_item_counter[item] += 1
-                if price_data[item][1][0]
+                if price_data.price_table[item][1].number == special_item_counter[item]:
+                    special_item_counter[item] = 0
+                    total += price_data.price_table[item][1].cost
+            else:
+                special_item_counter[item] = 1
         return total
     except Exception as e:
         return -1
 
-class SpecialOffer:
 
+class SpecialOffer:
+    """
+    An object that holds (n,y)
+    where n is number of that sku, and y is the cost in pounds
+    """
     def __init__(self, number, cost):
+        if number == 1:
+            raise ValueError("Number must be an integer greater than 1")
         self.number = number
         self.cost = cost
+
 
 class PriceTable:
     """
     Holds a price dictionary:
-    skus are the keys, values are a (price, special) tuple
-    price is a number, while special is a tuple of (n,y)
-    where n is n of that sku costs y pounds
-    if there is no special offer, special is None
+    skus are the keys, values are a (price, SpecialOffer) tuple
+    price is a number, while SpecialOffer is an object
+    if there is no special offer, the value is None
     """
     def __init__(self):
         special_A = SpecialOffer(3,130)
@@ -38,6 +48,7 @@ class PriceTable:
                             'B': (30, special_B),
                             'C': (20, None),
                             'D': (15, None)}
+
 
 
 

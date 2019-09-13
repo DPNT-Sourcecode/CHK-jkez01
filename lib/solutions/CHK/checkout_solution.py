8123f68a6f1ee787a330546053c1c23a079281bd
sbx_user1051 <sbx_user1051@169.254.68.53>
2019-09-13 05:20:57 +0000

    Fri Sep 13 05:20:57 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -116,33 +116,10 @@ class PriceTable:
     the special offer list is built and then sorted by favourability to the customer
     """
     def __init__(self):
-        table_text = """| A    | 50    | 3A for 130, 5A for 200 |
-| B    | 30    | 2B for 45              |
-| C    | 20    |                        |
-| D    | 15    |                        |
-| E    | 40    | 2E get one B free      |
-| F    | 10    | 2F get one F free      |
-| G    | 20    |                        |
-| H    | 10    | 5H for 45, 10H for 80  |
-| I    | 35    |                        |
-| J    | 60    |                        |
-| K    | 80    | 2K for 150             |
-| L    | 90    |                        |
-| M    | 15    |                        |
-| N    | 40    | 3N get one M free      |
-| O    | 10    |                        |
-| P    | 50    | 5P for 200             |
-| Q    | 30    | 3Q for 80              |
-| R    | 50    | 3R get one Q free      |
-| S    | 30    |                        |
-| T    | 20    |                        |
-| U    | 40    | 3U get one U free      |
-| V    | 50    | 2V for 90, 3V for 130  |
-| W    | 20    |                        |
-| X    | 90    |                        |
-| Y    | 10    |                        |
-| Z    | 50    |                        |"""
-        print(table_text)
+        with open('input.txt', 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                print(line)
         a_offers = [SpecialOffer('A', OfferType.DISCOUNT, (5, 200)),
                     SpecialOffer('A', OfferType.DISCOUNT, (3, 130))]
         a = Item('A', 50, a_offers)
@@ -205,23 +182,14 @@ def checkout(skus):
             else:
                 special_sku_counter[item] = 1
         for offer in table.special_offers:
-            print(offer.sku)
             while offer.is_applicable(special_sku_counter):
-                print('is applicable', total)
                 total += offer.get_cost(table.skus)
-                print('new total', total)
-                print(special_sku_counter)
                 special_sku_counter = offer.apply_special(special_sku_counter)
-                print(special_sku_counter)
-        print(total, special_sku_counter)
         for item in special_sku_counter:
             if special_sku_counter[item] != 0:
-                print(item)
                 total += special_sku_counter[item] * table.skus[item].get_cost()
-        print(total, special_sku_counter)
         return total
     except Exception as e:
-        print(e)
         return -1
 
 

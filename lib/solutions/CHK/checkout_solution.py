cc6c63449df8a091026cb0a6293d6716381bcfa8
sbx_user1051 <sbx_user1051@169.254.128.221>
2019-09-13 02:20:14 +0000

    Fri Sep 13 02:20:14 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,7 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    price_data = get_price_table_and_offers()
+    price_data = PriceTable()
     total = 0
     special_item_counter = {}
     try:
@@ -25,17 +25,20 @@ class SpecialOffer:
 
 class PriceTable:
     """
-    Return a price dictionary:
+    Holds a price dictionary:
     skus are the keys, values are a (price, special) tuple
     price is a number, while special is a tuple of (n,y)
     where n is n of that sku costs y pounds
     if there is no special offer, special is None
     """
     def __init__(self):
-        self.price_table = {'A': (50, (3,130)),
-                            'B': (30, (2,45)),
+        special_A = SpecialOffer(3,130)
+        special_B = SpecialOffer(2,45)
+        self.price_table = {'A': (50, special_A),
+                            'B': (30, special_B),
                             'C': (20, None),
                             'D': (15, None)}
+
 
 
 
